eef3a70b3481c0044a83e36a2952c4679f71407a
Ujjwal <ujjwal.mht89@gmail.com>
2025-05-09 20:01:45 +0530

    Fix: Correct Header/Footer import paths for Netlify

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import "./globals.css";
-import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header.js";
+import Footer from "./components/Footer/Footer.js";
 
 export const metadata = {
   title: "My Next.js App",
